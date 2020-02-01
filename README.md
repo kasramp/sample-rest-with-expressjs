03837e279e986401480282a4e35676ee1dc7bbf6
kasra.madadipouya <kasra.madadipouya@takeaway.com>
2020-02-01 21:41:44 +0100

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# sample-rest-with-expressjs
-Sample rest APIs built with ExpressJS and Node
+# Sample REST CRUD APIs with Node and Express.js
+
+This is a basic implementation of CRUD APIs with Express.js. It can be used as a start to build more complex APIs.
+
+## How to run
+
+### First time
+
+Clone the repository, go to the repository directory and then run,
+
+```bash
+$ npm install
+```
+
+### Run the project
+
+Run the below command,
+
+```bash
+$ npm run start
+```
+
+Then open the browser go to `http://localhost:8090/api/v1/users/`, you should be able to interact with the APIs.
+
+
---
+++ app.js
@@ -0,0 +1,89 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { restart } from "nodemon";
+
+const PORT = 8090;
+const app = express();
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.use("/api/v1/users/:id", function(req, res, next) {
+  if (req.params.hasOwnProperty("id") && !isNaN(parseInt(req.params.id))) {
+    next();
+  } else {
+    console.log("Request is invalid!");
+    res.status(400).send({ error: "User id should be a number" });
+  }
+});
+
+// Error handling for uncaught exceptions
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("Internal server error");
+});
+
+app
+  .route("/api/v1/users/:id")
+  .get(function(req, res) {
+    res.status(200).send({
+      userId: parseInt(req.params.id),
+      firstName: "John",
+      lastName: "Wick",
+      age: 45
+    });
+  })
+  .delete(function(req, res) {
+    let userId = parseInt(req.params.id);
+    res.status(204).send();
+  })
+  .put(function(req, res) {
+    let userId = parseInt(req.params.id);
+    const { firstName, lastName, age } = req.body;
+    const ageInt = parseInt(age);
+    if (firstName && lastName && ageInt) {
+      res.status(200).send({
+        userId: userId,
+        firstName: firstName,
+        lastName: lastName,
+        age: ageInt
+      });
+    } else {
+      res.status(400).send({
+        error: "The payload is wrong!"
+      });
+    }
+  });
+
+app.post(
+  "/api/v1/users",
+  (req, res, next) => {
+    const { firstName, lastName, age } = req.body;
+    let ageInt = parseInt(age);
+    if (firstName && lastName && ageInt) {
+      next();
+    } else {
+      res.status(400).send({
+        error: "The payload is wrong!"
+      });
+    }
+  },
+  (req, res) => {
+    const randomUserId = Math.floor(Math.random() * 65536);
+    res.status(201).send({
+      userId: randomUserId,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      age: parseInt(req.body.age)
+    });
+  }
+);
+
+app.listen(PORT, () => {
+  console.log("Server has started");
+});
